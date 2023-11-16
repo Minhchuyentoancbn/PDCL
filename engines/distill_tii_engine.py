e12239c070474209727e9ff8489543dd0505b9b6
Le Duc Minh <minhmeo7a1@gmail.com>
2023-11-16 15:47:01 +0700

    Add feature loss

--- engines/distill_tii_engine.py
+++ engines/distill_tii_engine.py
@@ -51,43 +51,29 @@ def train_one_epoch(model: torch.nn.Module, criterion, data_loader: Iterable, op
             not_mask = torch.tensor(not_mask, dtype=torch.int64).to(device)
             logits = logits.index_fill(dim=1, index=not_mask, value=float('-inf'))
 
-        # if args.use_auxillary_head:
-            # features = output['features']
+        if args.auxillary_prompt and full_prompt:
+            features = output['features']
 
-            # pretrained_res = model.get_query(input)
-            # pretrained_features = pretrained_res['features']
+            pretrained_res = model.get_query(input)
+            pretrained_features = pretrained_res['features']
             # pretrained_logits = pretrained_res['logits']
 
             # if args.train_mask and class_mask is not None:
             #     pretrained_logits = pretrained_logits.index_fill(dim=1, index=not_mask, value=float('-inf'))
 
-            # feature_criterion = nn.L1Loss()
+            feature_criterion = nn.L1Loss()
             # logits_criterion = nn.KLDivLoss(reduction='batchmean')
 
-            # main_loss = criterion(logits, target)
-            # feature_loss = feature_criterion(features, pretrained_features)
+            main_loss = criterion(logits, target)
+            feature_loss = feature_criterion(features, pretrained_features)
             # logits_loss = logits_criterion(nn.functional.log_softmax(logits[:, mask], dim=1),
             #                                nn.functional.softmax(pretrained_logits[:, mask], dim=1))
 
-            # if not math.isfinite(logits_loss.item()):
-            #     print("Logits loss is {}, stopping training".format(logits_loss.item()))
-            #     sys.exit(1)
+            loss = main_loss + feature_loss * args.auxillary_loss_lambda1 # + logits_loss * args.auxillary_loss_lambda2
 
-            # loss = main_loss + feature_loss * args.auxillary_loss_lambda1 + logits_loss * args.auxillary_loss_lambda2
+        else:
+            loss = criterion(logits, target)
 
-            # main_loss = criterion(logits, target)
-        # else:
-            # loss = criterion(logits, target)
-            # optimizer.zero_grad()
-            # loss.backward()
-            # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
-            # optimizer.step()
-
-            # if not math.isfinite(loss.item()):
-            #     print("Loss is {}, stopping training".format(loss.item()))
-            #     sys.exit(1)
-
-        loss = criterion(logits, target)
         optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
@@ -96,6 +82,16 @@ def train_one_epoch(model: torch.nn.Module, criterion, data_loader: Iterable, op
         if not math.isfinite(loss.item()):
             print("Loss is {}, stopping training".format(loss.item()))
             sys.exit(1)
+
+        # loss = criterion(logits, target)
+        # optimizer.zero_grad()
+        # loss.backward()
+        # torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm)
+        # optimizer.step()
+
+        # if not math.isfinite(loss.item()):
+        #     print("Loss is {}, stopping training".format(loss.item()))
+        #     sys.exit(1)
 
         acc1, acc5 = accuracy(logits, target, topk=(1, 5))
 
